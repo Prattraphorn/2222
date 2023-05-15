71632c43cb5e2c605148f2d842b7119ae42cbed8
Prattraphorn <122549764+Prattraphorn@users.noreply.github.com>
2023-05-15 16:47:34 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class Program {
+    static void Main() {
+        
+        Console.Write("Enter the amount of water: ");
+        float water = float.Parse(Console.ReadLine());
+
+        Console.Write("Enter the demand: ");
+        float demand = float.Parse(Console.ReadLine());
+
+        Console.Write("Enter the amount to refill: ");
+        float refill = float.Parse(Console.ReadLine());
+
+        if (refill > water) {
+            Console.WriteLine("Overflow water");
+        }
+        else {
+            float remainingWater = water - demand + refill;
+            float threshold = refill * 2;
+
+            if (remainingWater >= threshold) {
+                Console.WriteLine("Enough water! " + remainingWater + " left");
+            }
+            else {
+                Console.WriteLine("Not enough water");
+            }
+        }
+    }
+}
